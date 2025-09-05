d494d04627832ff2c5485071a76f1001d6210ecf
FranWeinmann <weinmann.fran@gmail.com>
2025-09-05 08:12:05 -0300

    .

--- main.js
+++ main.js
@@ -139,7 +139,7 @@ adjustDimensions();
 
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('/service-worker.js')
     .then(reg => console.log('Service Worker registrado:', reg))
     .catch(err => console.log('Error registrando SW:', err));
 }
