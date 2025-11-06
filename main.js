be933ac87c279fbccc6091c8f7d845437c46246a
FranWeinmann <weinmann.fran@gmail.com>
2025-11-06 12:02:20 -0300

    .

--- main.js
+++ main.js
@@ -105,10 +105,10 @@ function checkOption (){
 async function showCamera() {
   
   const videoUrl = `https://${raspbiID}/stream`;
-  // let imagen = await fetch(videoUrl, { 
-  //         method: "GET"
-  //       }).catch(err => { console.error("Error al enviar:", err) });
-  // console.log(imagen)
+  let imagen = await fetch(videoUrl, { 
+          method: "GET"
+        }).catch(err => { console.error("Error al enviar:", err) });
+  console.log(imagen)
   const img = document.createElement("img");
   img.id = "video-stream";
   img.alt = "Video de detecciones";
