c49ed408e2fca32453ecfdf1b642c9b9c55a95ad
FranWeinmann <166540403+FranWeinmann@users.noreply.github.com>
2025-11-12 08:26:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let isOn = true;
 let isHuman = true;
 let joystickInstance = null;
 let joystickCreated = false;
-const raspbiID = "unprudential-unrefreshed-bennie.ngrok-free";
+const raspbiID = "unprudential-unrefreshed-bennie.ngrok-free.dev";
 let frenar = false;
 
 function changeSelectOption (option, diselected){
